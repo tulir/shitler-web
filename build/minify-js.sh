9f5a09b81501646e4d1fe9bc722b8d6c4463b153
Tulir293 <tulir293@maunium.net>
2016-05-03 09:32:55 +0300

    Updated minify-js.sh and added index-min.html

--- build/minify-js.sh
+++ build/minify-js.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 cat \
-    ./js/lib/jquery.modal.min.js \
-    ./js/lib/linkify.min.js \
-    ./js/lib/linkify-html.min.js \
-    ./js/lib/linkify-jquery.min.js \
-    ./js/data.js \
-    ./js/vars.js \
     ./js/util.js \
-    ./js/channels.js \
-    ./js/autocomplete.js \
-    ./js/userlist.js \
-    ./js/messaging.js \
-    ./js/contextmenus.js \
-    ./js/settings.js \
-    ./js/scripts.js \
-    ./js/mauirc.js \
-    ./js/auth.js \
-    ./js/load.js \
+    ./js/receive.js \
+    ./js/join.js \
+    ./js/connection.js \
+    ./js/init.js \
     > ./index.js
 
 uglifyjs -cmo index.min.js -- index.js
